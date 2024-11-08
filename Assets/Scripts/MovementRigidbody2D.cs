49ee99d5807fb32ba922abddeb91e3f428a24678
Wizdid <BrandonT1722@gmail.com>
2024-11-08 18:03:25 -0500

    Platform Collisions
    Player now properly passes through platforms and stops when landing on them. The player can also fall through platforms by pressing/holding [S].

--- Assets/Scripts/MovementRigidbody2D.cs
+++ Assets/Scripts/MovementRigidbody2D.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -34,12 +35,20 @@ public class MovementRigidbody2D : MonoBehaviour
     
     private Rigidbody2D         rigid2D;
     private new Collider2D      collider2D;         // 현재 오브젝트의 충돌 범위 정보 
-    
+
+
+    private Collider2D tilemapCollider1;
+    private Collider2D tilemapCollider2;
+
     public bool IsLongJump { set; get; } = false;
+    public InfiniteTowerMovement towerScript1;
+    public InfiniteTowerMovement towerScript2;
 
     private void Awake(){
         rigid2D     = GetComponent<Rigidbody2D>();
         collider2D  = GetComponent<Collider2D>();
+        towerScript1 = GameObject.Find("Infinite Tower Tilemap").GetComponent<InfiniteTowerMovement>();
+        towerScript2 = GameObject.Find("Infinite Tower Tilemap Copy").GetComponent<InfiniteTowerMovement>();
     }
 
     private void FixedUpdate()
@@ -60,7 +69,7 @@ public class MovementRigidbody2D : MonoBehaviour
         // if player velocity.y > 0, then it means the player is in a process of jumping
         if (isGrounded && rigid2D.velocity.y <= 0)
         {
-            print("grounded");
+            //print("grounded");
             currentJumpCount = maxJumpCount;
         }
         
@@ -75,24 +84,45 @@ public class MovementRigidbody2D : MonoBehaviour
         {
             rigid2D.gravityScale = highGravity;
         }
+        //While the player is in the air, they will not collide with any platforms
+        if (!isGrounded)
+        {
+            towerScript1.turnOffPlatformCollision();
+            towerScript2.turnOffPlatformCollision();
+        }
     }
     public void MoveTo(float x)
     {
         rigid2D.velocity = new Vector2(x * moveSpeed, rigid2D.velocity.y);
     }
-
+    
     public bool JumpTo()
     {
-        
-        
+        //Jumping function; tracks number of jumps in the air
         if (currentJumpCount > 0)
         {
             rigid2D.velocity = new Vector2(rigid2D.velocity.x, jumpForce);
             currentJumpCount--;
+
+
             return true;
         }
 
         return false; 
+    }
+    public void OnTriggerEnter2D(Collider2D collision)
+    {
+        //If player collides with a platform while moving downwards and their feet are above the platform, they will stop at the platform
+        if (collision.tag == "Platform" && rigid2D.velocity.y <= 0) 
+        {   
+            if (footPosition.y > collision.ClosestPoint(transform.position).y)
+            {
+                towerScript1.turnOnPlatformCollision();
+                towerScript2.turnOnPlatformCollision();
+
+            }
+        }
+ 
     }
 
 }
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -8,9 +8,13 @@ public class PlayerController : MonoBehaviour
     [SerializeField]
     private KeyCode         jumpKey = KeyCode.Space;
 
+    public InfiniteTowerMovement towerScript1;
+    public InfiniteTowerMovement towerScript2;
     private void Awake()
     {
         movement2D = GetComponent<MovementRigidbody2D>();
+        towerScript1 = GameObject.Find("Infinite Tower Tilemap").GetComponent<InfiniteTowerMovement>();
+        towerScript2 = GameObject.Find("Infinite Tower Tilemap Copy").GetComponent<InfiniteTowerMovement>();
 
     }
     private void Update()
@@ -23,8 +27,19 @@ public class PlayerController : MonoBehaviour
     {
         float x = Input.GetAxisRaw("Horizontal");
         movement2D.MoveTo(x);
+        MoveDown();
     }
 
+    private void MoveDown()
+    {
+        //If the player presses/holds down [S], they will be able to move through platforms
+        if (Input.GetKey(KeyCode.S))
+        {
+            print("Test");
+            towerScript1.turnOffPlatformCollision();
+            towerScript2.turnOffPlatformCollision();
+        }
+    }
     private void UpdateJump()
     {
         if(Input.GetKeyDown(jumpKey))
@@ -39,6 +54,7 @@ public class PlayerController : MonoBehaviour
         {
             movement2D.IsLongJump = false; 
         }
+
     }
 
 
